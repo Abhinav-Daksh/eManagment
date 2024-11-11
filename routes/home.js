f319c13356d7f7a3d338fe11718e2ca9332e05fd
Praful-Daksh <prafuldaksh@proton.me>
2024-11-11 20:20:51 +0530

    update

--- routes/home.js
+++ routes/home.js
@@ -1,7 +1,7 @@
 import express from 'express';
 const router = express.Router();
 import Ticket from '../models/tickets.js';
-
+router.use(express.json());
 //home page
 router.get('/',(req,res)=>{
     res.render('home',{title:'ShowTime-home',cssFile: 'home.css'});
@@ -10,7 +10,7 @@ router.get('/shows',async(req,res)=>{
     try{
         const allTickets = await Ticket.find();
         if(allTickets){
-            res.render('home-shows',{title:'Explore-shows',cssFile:'home-shows.css',T:allTickets});
+            res.render('home-shows',{title:'Explore-shows',cssFile:'home-shows.css',head:'All Shows',T:allTickets});
         }else{
             res.render('home-shows',{title:'Explore shows',cssFile:'home-shows.css'});
         }
@@ -19,5 +19,20 @@ router.get('/shows',async(req,res)=>{
         res.status(502).send('Internal Server Error');
     }
 });
+router.post('/find-show',async(req,res)=>{
+    try{
+        const {searchValue} = req.body;
+        const searchedShows = await Ticket.find({showName:
+            {$regex: new RegExp(searchValue, 'i')}
+        });
+        res.render('home-shows',{title:'Explore-shows',cssFile:'home-shows.css',head:`Searched results for '${searchValue}'`,T:searchedShows});
+    }
+    catch(err){
+        console.log(err);
+        res.status(502).send('Internal Server Error');
+    }
+    
+
+})
 
 export default router;
